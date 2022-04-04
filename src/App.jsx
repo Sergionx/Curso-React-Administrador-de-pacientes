8a5fb11eb2dac6715dc3113487117f9d190d89d9
Sergionx <sergio180203@gmail.com>
2022-04-04 16:44:04 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListadoPacientes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListadoPacientes() {
+  return (
+    <div>
+      <h2>Listado de pacientes</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
